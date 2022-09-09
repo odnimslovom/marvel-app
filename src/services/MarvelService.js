29479be6ee0dd432d1cf376d8bafb152cd032a93
Drew Paramonov <odnimslovom@icloud.com>
2022-09-09 11:35:25 +0300

    Data transformation. Some optimisations

--- src/services/MarvelService.js
+++ src/services/MarvelService.js
@@ -3,6 +3,18 @@ class MarvelService {
     _apiBaseUrl = 'https://gateway.marvel.com:443/v1/public/';
     _apiToken = 'apikey=ad6f4475dffe94fda4246a92a833cb94';
 
+    _transformCharacterData = (character) => {
+      const defaultDescription = "Nothing description for this character.";
+      return ({
+        name: character.name,
+        description: character.description ? character.description : defaultDescription,
+        thumbnail:
+          character.thumbnail.path + '.' + character.thumbnail.extension,
+        homepage: character.urls[0].url,
+        wiki: character.urls[1].url,
+      });
+    }
+
      getResource = async (url) =>{
         let res = await fetch(url);
         if (!res.ok) {
@@ -11,12 +23,14 @@ class MarvelService {
         return await res.json();
     }
 
-    getAllCharacters = () =>{
-         return this.getResource(`${this._apiBaseUrl}characters?offset=0&limit=9&${this._apiToken}`);
+    getAllCharacters = async () =>{
+         const res = await this.getResource(`${this._apiBaseUrl}characters?offset=0&limit=9&${this._apiToken}`);
+         return res.data.results.map(this._transformCharacterData)
     }
 
-    getCharacter = (id) =>{
-         return this.getResource(`${this._apiBaseUrl}characters/${id}?offset=0&limit=9&${this._apiToken}`);
+    getCharacter = async(id) =>{
+        const res = await this.getResource(`${this._apiBaseUrl}characters/${id}?offset=0&limit=9&${this._apiToken}`);
+        return this._transformCharacterData(res.data.results[0]);
     }
 }
 
